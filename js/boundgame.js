672e40ce2f712a6b9a4832b2f7dbe62e46c91c26
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-09-01 09:43:53 +0900

    docs: BOUND BALL GAME SERVICE - JSDOC

--- js/boundgame.js
+++ js/boundgame.js
@@ -3,6 +3,10 @@ const paddle = new DrawPaddle;
 const brick = new DrawBrick;
 let over = false;
 
+/**
+ * @function liveBall
+ * @description 공 속도 나 튕김등을 제어하면서 지속적으로 움직이는 공을 그린다
+ */
 function liveBall () {
     const ballSpeedLimit = ((ball.canvas.width + ball.canvas.height) / 250);
     const ballRandom = Math.random() * (Math.random() * ballSpeedLimit) ;
@@ -47,7 +51,10 @@ function liveBall () {
     ball.x += ball.moveX;
     ball.y += ball.moveY;
 }
-
+/**
+ * @function getPaddle
+ * @description 공을 받아 칠 채를 그리고 이벤트에 따라 이동시킨다
+ */
 function getPaddle () {
     const paddleSpeed = (paddle.canvas.width / 110);
     paddle.drawPaddle();
@@ -59,6 +66,11 @@ function getPaddle () {
     paddle.x -= paddleSpeed;
     }
 }
+/**
+ * @function checkGameOver
+ * @description 공이 아래 선 위치에 도착했을때 채(paddle)의 위치를 비교해서 공을 튀기거나 실패에 대한 처리를 한다
+ * @param {Float} ballRandom 
+ */
 function checkGameOver (ballRandom) {
     if((ball.x + ball.ballRadius - 2) > paddle.x && (ball.x - ball.ballRadius + 2) < (paddle.x + paddle.width)){
         ball.moveY = -ball.moveY + ballRandom;
@@ -73,7 +85,10 @@ function checkGameOver (ballRandom) {
 
     ball.colorIndex++;
 }
-
+/**
+ * @function getBrick
+ * @description 벽돌 규격에 맞게 벽돌 2차원 배열을 생성한다
+ */
 function getBrick () {
     for(let c = 0; c < brick.column; c++){
         brick.box[c] = [];
@@ -82,8 +97,11 @@ function getBrick () {
         }
     }
 }
-
-function drawBirck () {
+/**
+ * @function drawBrick
+ * @description get 해온 벽돌 배열 상태에 맞게 벽돌을 그린다
+ */
+function drawBrick () {
     for(let c = 0; c < brick.column; c++){
         for(let r = 0; r < brick.row; r++){
             if(brick.box[c][r].status === 1){
@@ -97,6 +115,10 @@ function drawBirck () {
         }
     }
 }
+/**
+ * @function crashBrick
+ * @description 공과 벽돌사이의 충돌을 감지하고 충돌 이벤트를 발생 시킨다
+ */
 function crashBrick () {
     for(let c = 0; c < brick.column; c++) {
         for(let r = 0; r < brick.row; r++) {
@@ -111,15 +133,21 @@ function crashBrick () {
         }
     }
 }
-
+/**
+ * @function draw
+ * @description canvas내용물을 request 애니메이션을 통해서 지속적으로 그린다
+ */
 function draw () {
     liveBall();
     getPaddle ();
-    drawBirck();
+    drawBrick();
     crashBrick ();
     requestAnimationFrame(draw);
 }
-
+/**
+ * @function init
+ * @description 즉시 실행함수 이벤트 리스닝 등 1회성 함수들과 draw 함수의 실행을 담당한다
+ */
 (function init (){
     paddle.keyEvent();
     paddle.mouseEvent();
